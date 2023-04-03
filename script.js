7e1ba04176417fcec73922df40c29bb8f26898e2
IliaMara <ilia.marakhin@gmail.com>
2023-04-03 22:24:11 +0400

    Added removing element

--- script.js
+++ script.js
@@ -9,9 +9,18 @@ input.addEventListener("keydown", function (event){
 function addItem(){
     let divParent = document.createElement("div");
     let divChild = document.createElement("div");
+    let trashIcon = document.createElement("div");
 
     divParent.className = "item";
     divParent.innerHTML = "<div>" + input.value + "</div>";
+
+    trashIcon.className = "fas fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click", function (){
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashIcon);
 
     divParent.appendChild(divChild);
 
