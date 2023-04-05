c89ee11d277e58fd1fa5e5728805c7dddf16d0b5
IliaMara <ilia.marakhin@gmail.com>
2023-04-05 17:38:02 +0400

    Replaced creating todoDiv on updating based todo list

--- script.js
+++ script.js
@@ -9,39 +9,69 @@ input.addEventListener("keydown", function (event){
     if (event.key === "Enter")
         addItem();
 })
+updateTodoDocument();
 
 function addItem(){
-    let divParent = document.createElement("div");
-    let divChild = document.createElement("div");
-    let checkIcon = document.createElement("i");
-    let trashIcon = document.createElement("div");
+    if (input.value) {
+        TODOS.push(
+            new Todo(uid(), input.value, false)
+        );
 
-    divParent.className = "item";
-    divParent.innerHTML = "<div>" + input.value + "</div>";
+        input.value = '';
 
-    checkIcon.className = "fas fa-check-square";
-    checkIcon.style.color = lightGrayColor;
-    checkIcon.addEventListener("click", function (){
-        if (checkIcon.style.color === lightGrayColor) {
-            checkIcon.style.color = greenColor;
-        } else {
-            checkIcon.style.color = lightGrayColor;
-        }
-    })
-
-    divChild.appendChild(checkIcon);
-
-    trashIcon.className = "fas fa-trash";
-    trashIcon.style.color = darkGrayColor;
-    trashIcon.addEventListener("click", function (){
-        divParent.remove();
-    })
-
-    divChild.appendChild(trashIcon);
-
-    divParent.appendChild(divChild);
-
-    toDoItems.appendChild(divParent);
-
-    input.value = '';
+        updateTodoDocument();
+    }
 }
+
+function removeItem(id) {
+    TODOS = TODOS.filter(todo => todo.id !== id);
+    updateTodoDocument();
+}
+
+function switchStatus(id) {
+    TODOS.filter(todo => todo.id === id)
+        .forEach(todo => todo.isDone = !todo.isDone);
+    updateTodoDocument();
+}
+
+
+function updateTodoDocument() {
+    let children = [];
+    TODOS.forEach(todo => {
+        let divParent = document.createElement("div");
+        let divChild = document.createElement("div");
+
+        let trashIcon = document.createElement("div");
+        let checkIcon = document.createElement("i");
+
+        trashIcon.className = "fas fa-trash";
+        trashIcon.style.color = darkGrayColor;
+        trashIcon.addEventListener("click", function (){
+            removeItem(todo.id);
+        })
+
+        divChild.appendChild(trashIcon);
+
+        checkIcon.className = "fas fa-check-square";
+        checkIcon.style.color = todo.isDone ? greenColor : lightGrayColor;
+        checkIcon.addEventListener("click", function (){
+            switchStatus(todo.id);
+        })
+
+        divChild.appendChild(checkIcon);
+
+        divParent.className = "item";
+        divParent.innerHTML = todo.description;
+
+        divParent.appendChild(divChild);
+
+        children.push(divParent);
+    })
+    toDoItems.replaceChildren(...children);
+    console.log(TODOS);
+}
+
+function uid(){
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
